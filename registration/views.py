7aff01e42b31e3f53b76e85594d9f4487cdcfef5
Mansi Gupta <44631632+Mansi145@users.noreply.github.com>
2020-02-21 00:37:27 +0530

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -6,9 +6,3 @@ from . import forms
 
 # Create your views here.
 
-class userLogin(TemplateView):
-	template_name = 'base.html'
-
-	def get(self, request):
-		form = forms.loginForm()
-		return render(request, self.template_name, {'form':form})
