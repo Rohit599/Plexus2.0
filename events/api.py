0216c0017c6267333bd4b760abdcb50306a46402
PragatiVerma18 <itispragativerma.com>
2020-03-03 23:59:36 +0530

    added serializers

--- events/api.py
+++ events/api.py
@@ -2,6 +2,8 @@ from events.models import Event
 from rest_framework import viewsets, permissions
 from .serializers import EventSerializer
 from registration.models import society
+from .models import Event, Question, Answers, Score, Rule
+from .serializers import QuestionSerializer, AnswerSerializer, ScoreSerializer, RuleSerializer
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
@@ -13,3 +15,47 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.request.user.events.all()
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        return self.request.user.question.all()
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answers.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = AnswerSerializer
+
+    def get_queryset(self):
+        return self.request.user.answer.all()
+
+class ScoreViewSet(viewsets.ModelViewSet):
+    queryset = Score.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = ScoreSerializer
+
+    def get_queryset(self):
+        return self.request.user.score.all()
+
+class RuleViewSet(viewsets.ModelViewSet):
+    queryset = Rule.objects.all()
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+
+    serializer_class = RuleSerializer
+
+    def get_queryset(self):
+        return self.request.user.rule.all()
