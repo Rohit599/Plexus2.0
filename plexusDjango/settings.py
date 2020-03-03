f4c85160d74959b189bcf26ee5bd25b16316993c
pragati <itispragativerma@gmail.com>
2020-03-04 02:13:33 +0530

    minor fixes

--- plexusDjango/settings.py
+++ plexusDjango/settings.py
@@ -19,7 +19,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["https://ncs-plexus.herokuapp.com/", "http://127.0.0.1:8000" ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8000",
+    "http://localhost:3000",
     "http://127.0.0.1:8000",
     "https://ncs-plexus.herokuapp.com/"
 ]
